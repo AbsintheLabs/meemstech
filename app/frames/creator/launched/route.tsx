eb463dac63033d2828def480823d3511a13888c2
Ketan Jog <k@absinthelabs.xyz>
2024-03-24 01:45:56 -0400

    teed up kv store

--- app/frames/creator/launched/route.tsx
+++ app/frames/creator/launched/route.tsx
@@ -1,18 +1,17 @@
 import { createFrames, Button } from "frames.js/next";
+import { kv } from "@vercel/kv";
 
 // not ideal import paths for now
 import { processImages } from "../../../utils/processImages";
 
 const frames = createFrames({
   basePath: "/frames",
-  initialState: {
-  },
+  initialState: {},
 });
 
 const handleRequest = frames(async (ctx) => {
   // TODO: create something that will autogenerate this
   const launchedLink = "https://meems.tech/@absinthe/WIF";
-
 
   // --------- OLD BELOW ------------
   // image url that will be displayed
@@ -21,17 +20,22 @@ const handleRequest = frames(async (ctx) => {
   const nameSelector = ctx.searchParams.nameSelector || "name";
   // process images to make it square and compatible with frames
   const [processedImageUrl] = await processImages([selectedImageUrl]);
-  const textPrompt = nameSelector === "name" ? "name of meme (ex: dogwifhat)" : "ticker of meme (ex: WIF)";
-  console.log("selectedImageUrl", selectedImageUrl)
+  const textPrompt =
+    nameSelector === "name"
+      ? "name of meme (ex: dogwifhat)"
+      : "ticker of meme (ex: WIF)";
+  console.log("selectedImageUrl", selectedImageUrl);
+
+  // key = "creatorAddress/ticker/img" value=processedImageUrl
+  // const key = `${ctx?.message?.castId.fid}/${NEED TO ADD THE TICKER HERE}/img`;
+  // const saveImage = await kv.set(key, processedImageUrl);
 
   return {
     image: (
       // TODO: explain how the user can add handles here and for what
       <div tw="flex flex-col">
         <img width={220} height={220} src={processedImageUrl} alt="Image" />
-        <div tw="flex">
-          launched screen
-        </div>
+        <div tw="flex">launched screen</div>
       </div>
     ),
     buttons: [
@@ -40,18 +44,14 @@ const handleRequest = frames(async (ctx) => {
         action="link"
         target={{
           query: {},
-          href: launchedLink
+          href: launchedLink,
         }}
       >
         Repost the warp! 🚀
-      </Button >,
-
+      </Button>,
     ],
   };
 });
 
 export const GET = handleRequest;
 export const POST = handleRequest;
-
-
-
