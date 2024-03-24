359bee0efa657ab602276120136cb8ab8c1a0852
Andrew Magid <magid.andrew@gmail.com>
2024-03-24 04:47:22 -0400

    changed text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Run this page in a warpcast to see it work!</div>;
+  return <div>Run https://fremes.wtf/frames in warpcast to see it work!</div>;
 }
